e2e9ad9be4501e6496332be806482ba147eb2d8c
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-25 21:28:37 +0000

    add

--- src/server.js
+++ src/server.js
@@ -1,50 +1,15 @@
+//requiring path and fs modules etc
 const express = require('express');
 const multer  = require('multer');
 const fs  = require('fs');
 const parser = require("todotxt-parser");
 const { parse } = require('@lggruspe/todo-txt-parser');
 const { readFileSync } = require('fs')
-
-//requiring path and fs modules
 const path = require('path');
-//joining path of directory 
-const directoryPath = path.join(__dirname, '../data');
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    console.log("directoryPath", directoryPath);
-    //listing all files using forEach
-    var filenamepath;
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        filenamepath= file;
-        if (filenamepath.endsWith("txt")){
-            const filepathnew = directoryPath+"/"+filenamepath;
-            fs.readFile(filepathnew, {encoding: 'UTF-8'}, (err, data) => {
-                const todo = readFileSync(filepathnew, 'utf8').trim() // '../app/uploads/todo.txt'
-                if (err) { // or if (err) throw err;
-                    console.error(err);
-                    return;
-                  }
-                //console.log(data);
-                console.log(parser.relaxed(data));
-                //console.log(parse(todo));
-              });
-            //console.log(filenamepath); 
-        }
-        else {
-          return null; //console.log("out extension:", filenamepath); 
-        }
-    });
-});
-
+const directoryPath = path.join(__dirname, '../data'); //joining path of directory 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -69,7 +34,35 @@ app.post('/upload', function (req, res, next) {
         if (err) {
             return res.end("Something went wrong:(");
         }
-        res.end("Upload completed.");
+        else {
+            fs.readdir(directoryPath, function (err, files) { //passsing directoryPath and callback function
+                if (err) {//handling error
+                    return console.log('Unable to scan directory: ' + err);
+                } //console.log("directoryPath", directoryPath); //listing all files using forEach
+                var filenamepath;
+                files.forEach(function (file) {
+                    // Do whatever you want to do with the file
+                    filenamepath= file;
+                    if (filenamepath.endsWith("txt")){
+                        const filepathnew = directoryPath+"/"+filenamepath;
+                        fs.readFile(filepathnew, {encoding: 'UTF-8'}, (err, data) => {
+                            const todo = readFileSync(filepathnew, 'utf8').trim() // '../app/uploads/todo.txt'
+                            //var dataFileUpload;
+                            if (err) { // or if (err) throw err;
+                                console.error(err);
+                                return;
+                              } //console.log(data); //dataFileUpload = parser.relaxed(data); //return dataFileUpload; //return res.render('index', {dataFileUpload: parser.relaxed(data), tagline: tagline}); //console.log(parser.relaxed(data));  //console.log(parse(todo));
+                            // return parser.relaxed(data);
+                            //res.render('index', {dataFileUpload: parser.relaxed(data), tagline: tagline});
+                            res.end("Upload completed."+JSON.stringify(parser.relaxed(data)));
+                          }); //console.log(filenamepath); 
+                    }
+                    else {
+                      return null; //console.log("out extension:", filenamepath); 
+                    }
+                });
+            });
+        }
     });
 })
 
